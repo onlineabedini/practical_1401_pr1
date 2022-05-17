de32e185596efbd5a6d38c3cf3245e35f98cac69
AlirezaAbedini <onlineabedini@gmail.com>
2022-05-17 22:56:43 +0430

    fontawesome added

--- personal/nuxt.config.js
+++ personal/nuxt.config.js
@@ -11,7 +11,8 @@ export default {
             { hid: 'description', name: 'description', content: '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'stylesheet', type: 'text/css', href: 'https://use.fontawesome.com/releases/v5.15.4/css/all.css' },
         ]
     },
 
