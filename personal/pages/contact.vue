ecb857a7a37644cbefeb0fdc310540059c5e749b
armina <arminaghanadian@gmail.com>
2022-05-19 14:44:52 +0430

    contact page completed

---
+++ personal/pages/contact.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+  <div class=p-5></div>
+    
+    
+    
+    <p class="text-center font-weight-normal h3">ارتباط با من</p>
+    <div class=p-3></div>
+
+    <div class="input-group mb-3 p-1 ">
+  <div class="input-group-prepend p-3 ">
+    
+  </div>
+  <input type="text" class="form-control" placeholder=" نام و نام خانوادگی را وارد کنید" >
+    </div>
+    <div class=p-2></div>
+   <div class="input-group mb-3 p-1">
+  <div class="input-group-prepend p-3">
+    
+  </div>
+  <input type="text" class="form-control" placeholder="ایمیل را وارد کنید " >
+
+    </div>
+    <div class=p-2></div>
+    <div class="input-group p-1 ">
+  <div class="input-group-prepend p-3">
+    
+  </div>
+  <input type="text" class="form-control" placeholder="پیام را وارد کنید " >
+  
+</div>
+
+    <div class="p-3"></div>
+    
+    <button type="button" class="btn btn-primary d-grid gap-1 col-1 mx-auto ">ارسال</button>
+
+
+
+
+    <div class=" p-5 "></div>
+    
+    
+      <div ></div> 
+        
+        
+        
+
+        </div>
+
+    
+    
+
+
+
+
+</template>
