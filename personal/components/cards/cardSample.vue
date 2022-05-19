290f3b25f857b80159d4eab4b846e16dec7c2042
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-05-20 02:21:24 +0430

    product card build up but not ready yet

--- personal/components/cards/cardSample.vue
+++ personal/components/cards/cardSample.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div>
+    <div>
+      <img :src="cardImage" class="card-img img-size mx-4" alt="image" />
+    </div>
+
+    <div>
+      <h6 class="card-title title-align">
+        {{ cardTitle }}
+      </h6>
+    </div>
+
+    <div>
+      <p class="card-text">
+        {{ decription }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "cardSample",
+  props: [
+    "cardImage",
+    "cardTitle",
+    "decription",
+  ],
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- personal/pages/sample.vue
+++ personal/pages/sample.vue
@@ -1,13 +1,47 @@
 <template>
-  
+  <div class="container p-5 card border">
+    <div class="row">
+      <div class="col-3" v-for="card in cardsList" :key="card.cardTitle">
+        <cardSample
+          :cardImage="card.cardImage"
+          :cardTitle="card.cardTitle"
+          :decription="card.decription"
+        ></cardSample>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import cardSample from "~/components/cards/cardSample.vue";
 export default {
-
-}
+  name: "cardSample",
+  components: {
+    cardSample,
+  },
+  data() {
+    return {
+      cardsList: {
+        card1: {
+          cardImage: "@/static/img/1.jpg",
+          cardTitle: "Card title",
+          decription: "example description",
+        },
+        card2: {
+          cardImage: "@/static/img/2.jpg",
+          cardTitle: "Card title",
+          decription: "example description",
+        },
+        card3: {
+          cardImage: "@/static/img/3.jpg",
+          cardTitle: "Card title",
+          decription: "example description",
+        },
+      },
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
