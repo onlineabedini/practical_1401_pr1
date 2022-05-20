a053288296984d59047837e68e4497fc99719ec2
unknown <negar.mzr.1380@gmail.com>
2022-05-20 18:17:26 +0430

    card blog is created

--- personal/pages/blog.vue
+++ personal/pages/blog.vue
@@ -1,11 +1,18 @@
 <template>
-
+<div>
+<cardBlog></cardBlog>
+</div>
 </template>
 
 <script>
+import cardBlog from "~/components/cards/cardBlog.vue";
 export default {
-
+    components: {
+        cardBlog,
+    
+    },
 }
+
 </script>
 
 <style>
