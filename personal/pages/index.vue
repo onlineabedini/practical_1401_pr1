b2b3627075e1a94166a97cd252fdb3396395d951
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-05-28 19:38:18 +0430

    api in index page for articles.

--- personal/pages/index.vue
+++ personal/pages/index.vue
@@ -7,7 +7,7 @@
           <div class="border p-4">
             <div class="border border-3 rounded"><h4>مهارت های من</h4></div>
 
-            <div class="progress my-2 ">
+            <div class="progress my-2">
               <div
                 class="progress-bar progress-bar-striped"
                 role="progressbar"
@@ -61,28 +61,30 @@
           <!-- about div  -->
           <div class="border my-2 p-4">
             <div class="border border-3 rounded"><h4>درباره ی من</h4></div>
-            <div class="py-2"><p>
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-              استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
-              نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-              کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان
-              جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای
-              طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان
-              فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری
-              موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد
-              نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل
-              دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
-            </p></div>
+            <div class="py-2">
+              <p>
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
+                نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+                کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان
+                جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را
+                برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در
+                زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و
+                دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد
+                وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات
+                پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+            </div>
           </div>
           <div class="border p-4">
             <div class="border border-3 rounded"><h4>پست‌های وبلاگ</h4></div>
             <div class="row row-cols-4 py-4">
-              <div class="col" v-for="card in cardsList" :key="card.id">
+              <div class="col" v-for="article in article" :key="article.title">
                 <cardBlog
-                  :cardImage="card.cardImage"
-                  :cardTitle="card.cardTitle"
-                  :description="card.description"
+                  :title="article.title"
+                  :description="article.description"
+                  :image="article.image"
                 ></cardBlog>
               </div>
             </div>
@@ -108,7 +110,11 @@
     </div>
   </div>
 </template>
+
+
 <script>
+import axios from "axios";
+
 import cardSample from "@/components/cards/cardSample.vue";
 import cardBlog from "~/components/cards/cardBlog.vue";
 export default {
@@ -119,31 +125,45 @@ export default {
     cardSample,
     cardBlog,
   },
+
   data() {
     return {
-      cardsList: {
-        card1: {
-          cardImage: "img/1.jpg",
-          cardTitle: "Card title 1",
-          description: "example description 1",
-        },
-        card2: {
-          cardImage: "img/2.jpg",
-          cardTitle: "Card title 2",
-          description: "example description 2",
-        },
-        card3: {
-          cardImage: "img/3.jpg",
-          cardTitle: "Card title 3",
-          description: "example description 3",
-        },
-        card4: {
-          cardImage: "img/3.jpg",
-          cardTitle: "Card title 4",
-          description: "example description 4",
-        },
-      },
+      article: [],
+      // cardsList: {
+      //   card1: {
+      //     cardImage: "img/1.jpg",
+      //     cardTitle: "Card title 1",
+      //     description: "example description 1",
+      //   },
+      //   card2: {
+      //     cardImage: "img/2.jpg",
+      //     cardTitle: "Card title 2",
+      //     description: "example description 2",
+      //   },
+      //   card3: {
+      //     cardImage: "img/3.jpg",
+      //     cardTitle: "Card title 3",
+      //     description: "example description 3",
+      //   },
+      //   card4: {
+      //     cardImage: "img/3.jpg",
+      //     cardTitle: "Card title 4",
+      //     description: "example description 4",
+      //   },
+      // },
     };
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:2020/api/article")
+      .then((response) => {
+        this.article = response.data;
+        console.log(this.article);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
 };
 </script>
