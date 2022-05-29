5510fbfcc583100fdc5942f3d38577da95843189
reza <reza.pk.lu9@gamil.com>
2022-05-29 18:00:32 +0430

    saba s task in created

---
+++ personal/pages/article.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <handout></handout>
+  </div>
+</template>
+
+<script>
+import handout from "@/components/handout.vue";
+export default {
+  components: {
+    handout,
+  },
+};
+</script>
+
+<style>
+</style>
