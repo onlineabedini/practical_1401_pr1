487f7b0ab27024a8bcd6340202f2ec2db568c2fe
AlirezaAbedini <onlineabedini@gmail.com>
2022-07-05 14:51:50 +0430

    Update nuxt.config.js

--- commercial/nuxt.config.js
+++ commercial/nuxt.config.js
@@ -1,20 +1,5 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'فروشگاه مطالعاتی آی آر نود',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: ['@/assets/css/bootstrap.min.css', '@/assets/css/style.css', '@/assets/css/main.css'],
 
