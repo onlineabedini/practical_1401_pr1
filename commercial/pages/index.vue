f2a5a7f584dcfdd0661083c1003e4bb4992841a3
aliqasemii <alighasmi2000@gamil.com>
2022-07-20 17:53:48 +0430

    Footer text is done

--- commercial/pages/index.vue
+++ commercial/pages/index.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
   <index></index>
+  <app_footer></app_footer>
 </div>
   
 </template>
 
 <script>
 import index from "@/components/header/index.vue"
+import app_footer from '@/components/footer'
 export default {
   components :{
-    index
+    index,
+    app_footer
   }
 
 }
