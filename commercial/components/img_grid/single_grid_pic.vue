049a9a0f51f2a32c9ef484a90702c97b58b66779
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-07-20 21:33:24 +0430

    single-grid-pic was 90 percent done

--- commercial/components/img_grid/single_grid_pic.vue
+++ commercial/components/img_grid/single_grid_pic.vue
@@ -1,13 +1,30 @@
 <template>
-  
+  <div class="row">
+    <div class="col">
+      <div>
+        <h5 class="card-title p-3 mt-5">
+          {{ item.cardTitle }}
+        </h5>
+      </div>
+      <div>
+        <h4 class="p-3">
+          {{ item.text }}
+        </h4>
+      </div>
+    </div>
+
+    <div class="col d-flex m-auto">
+      <img :src="item.cardImage" class="img-fluid" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["item"],
+  name: "single_grid_pic",
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
