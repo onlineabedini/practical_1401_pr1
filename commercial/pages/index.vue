fe8f7fd69c7dc55150531b2d88849d511fe3c506
reza1899 <95365529+reza1899@users.noreply.github.com>
2022-07-22 12:18:03 +0430

    second grid component is created and also footer and header import to main page

--- commercial/pages/index.vue
+++ commercial/pages/index.vue
@@ -1,18 +1,23 @@
 <template>
 <div>
+  <app_header></app_header>
     <index></index>
+    <app_footer></app_footer>
   </div>
 </template>
 
 <script>
-
-import Index from '@/components/img_grid/index.vue';
+import app_header from '@/components/header'
+import index from '@/components/img_grid/index.vue';
+import app_footer from '@/components/footer';
 export default {
-  layout() {
-    return "single_grid_pic";
-  },
+  // layout() {
+  //   return "single_grid_pic";
+  // },
   components: {
-    Index,
+    index,
+    app_header,
+    app_footer,
   },
 };
 </script>
