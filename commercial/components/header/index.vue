a1e3f4035c24767cab2452b5506f81d573f4beb6
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-07-23 02:51:00 +0430

    mega menu was created but has the vertical problem

--- commercial/components/header/index.vue
+++ commercial/components/header/index.vue
@@ -1,69 +1,59 @@
 <template>
   <div>
     <div class="border border-3 row bg-dark">
-        <div class="col-2 "></div>
-         
-      <div class="col-8  ">
+      <div class="col-2"></div>
 
-      <div class="d-flex">
-        <a href="#" class="text-decoration-none">
-          <h3 class="text-white mx-4 fw-bold pt-2">m</h3>
-        </a>
-        <a href="#" class="text-decoration-none text-white mx-2 pt-2"> خانه </a>
+      <div class="col-8">
+        <div class="d-flex">
+          <a href="#" class="text-decoration-none">
+            <h3 class="text-white mx-4 fw-bold pt-2">m</h3>
+          </a>
+          <a href="#" class="text-decoration-none text-white mx-2 pt-2">
+            خانه
+          </a>
 
-        <div class="dropdown show mx-4 pt-2">
-          <a
-            class="btn btn-secondary dropdown-toggle"
-            href="#"
-            role="button"
-            id="dropdownMenuLink"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            خدمات ما
+          <div class="dropdown show mx-4 pt-2">
+            <Main_menu></Main_menu>
+          </div>
+          <a href="#" class="text-decoration-none text-white mx-2 pt-2">
+            وبلاگ
+          </a>
+          <div class="dropdown show mx-4 pt-2">
+            <a
+              class="btn btn-secondary dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              درباره ما
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="d-flex justify-content-end">
+          <a href="#" class="text-decoration-none text-white mx-4 py-2">
+            عضویت
+          </a>
+          <a href="#" class="text-decoration-none text-white mx-4 py-2">
+            وارد شوید
           </a>
         </div>
-        <a href="#" class="text-decoration-none text-white mx-2 pt-2">
-          وبلاگ
-        </a>
-        <div class="dropdown show mx-4 pt-2">
-          <a
-            class="btn btn-secondary dropdown-toggle"
-            href="#"
-            role="button"
-            id="dropdownMenuLink"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            درباره ما
-          </a>
-        </div>
-        </div>
       </div>
-       <div class="col-2 ">
-           <div class="d-flex justify-content-end">
-             <a href="#" class="text-decoration-none text-white mx-4 py-2 ">
-                عضویت
-        </a> 
-         <a href="#" class="text-decoration-none text-white mx-4 py-2 ">
-                 وارد شوید
-                 </a>
-           </div>
-       </div>
-      
-      </div>
-
-   
-      
-      </div>
-    
- 
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Main_menu from "@/components/menu/main_menu/index.vue";
+export default {
+  components: {
+    Main_menu,
+  },
+};
 </script>
 
 <style>
