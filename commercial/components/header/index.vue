5dc00a42f20d2d459b0b3a8dc032204b35c5ce8b
aliqasemii <alighasmi2000@gamil.com>
2022-07-23 17:14:20 +0430

    The first page is finished

--- commercial/components/header/index.vue
+++ commercial/components/header/index.vue
@@ -44,7 +44,7 @@
       </div>
        <div class="col-2 ">
            <div class="d-flex justify-content-end">
-             <a href="#" class="text-decoration-none text-white mx-4 py-2 ">
+             <a href="commercial\pages\about_us\about_irnode.vue" class="text-decoration-none text-white mx-4 py-2 ">
                 عضویت
         </a> 
          <a href="#" class="text-decoration-none text-white mx-4 py-2 ">
