813e9d9e69c3d461b9fd8a8f01cab919cbec8fb1
armina <arminaghanadian@gmail.com>
2022-07-24 23:03:29 +0430

    _id.vue completed

--- commercial/pages/product/single_product/_id.vue
+++ commercial/pages/product/single_product/_id.vue
@@ -1,9 +1,16 @@
 <template>
+<div>
+  <addtocart></addtocart>
+</div>
   
 </template>
 
 <script>
+import add_to_cart from "@/components/commerce_components/single_product/add_to_cart_component.vue"
 export default {
+  component :{
+    add_to_cart,
+  }
 
 }
 </script>
