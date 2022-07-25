231ff15551d6c4cd38e036e6625d2d29832d8891
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-07-25 17:09:13 +0430

    Update single_card.vue

--- commercial/components/commerce_components/products_grid/single_card.vue
+++ commercial/components/commerce_components/products_grid/single_card.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
+    <i class="text-white bg-danger fa"><p>discount</p></i>
     <img :src="product.cardImage" class="img-fluid d-table" alt="img" />
 
-    <h3 class="text-center text-dark ">
+    <h3 class="text-center text-dark">
       {{ product.cardTitle }}
     </h3>
     <h5 class="text-center text-secondary">
       {{ product.price }}
     </h5>
+    <!-- plus icon -->
+    <div class="d-flex justify-content-center">
+      <div class="btn rounded-pill btn-danger d-grid col-md-2">
+        <i class="fa fa-plus" aria-hidden="true"></i>
+      </div>
+    </div>
   </div>
 </template>
 
