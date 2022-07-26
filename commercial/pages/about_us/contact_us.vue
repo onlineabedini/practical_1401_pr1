48842fcf40aeaf6c585cbb7e3156662242a47019
aliqasemii <alighasmi2000@gamil.com>
2022-07-26 20:02:08 +0430

    tak done

--- commercial/pages/about_us/contact_us.vue
+++ commercial/pages/about_us/contact_us.vue
@@ -1,5 +1,123 @@
 <template>
-  
+  <div>
+      <div>
+          <h1 class="text-center pt-5">
+              ارتباط با ما
+          </h1>
+      </div>
+      <div >
+          <p class="text-center pt-5 t1">
+              در راستای ارتقا سطح کیفیت و پشتبانی وبسایت و همچنین ماهیت محصولات ارایه شده ، تصمیم گرفتیم تا پشتیبانی آنلاین وبسایت را بصورت 12 ساعته راه اندازی کنیم و از این پس می توانید برای حل مشکلات خود از طریق سامانه <strong>پشتیبانی</strong> آنلاین سایت خدمات خود را دریافت نمایید.همچنین اگر بر حسب اتفاق سامانه چت آنلاین با نقص فنی مواجه شد می توانید از طریق راه های ارتباطی زیر با ما در تماس باشید ، همکاران ما بی درنگ پاسخگوی شما خواهند بود.
+          </p>
+      </div>
+      <div class="row d-flex t4 px-5 t5">
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  py-4">
+                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
+                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
+                      
+                      <p class="pt-3 text-danger"><b>سامانه گفتگو انلاین</b></p>
+                      <p class="text-primary">پشتیبانی آنلاین وبسایت</p>
+                      <i>
+                        
+                      </i>
+
+                  </div>
+
+              </a>
+          </div>
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  py-4">
+
+                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
+                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
+                      
+                      <p class="pt-3 text-danger"><b>@abc-123</b></p>
+                      <p class="text-primary">پشتیبانی تلگرام</p>
+
+                  </div>
+
+              </a>
+          </div>
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  py-4">
+                      <p class="text-dark"> پشتیبانی سفارش، انتقادات و پیشنهادات </p>
+                      <p class="text-dark">(روز های کاری 9 الی 14)</p>
+
+                      <p class="pt-3 text-danger"> <b>00 00 0000_031</b></p>
+                      <p class="text-primary">11 11 1111_031</p>
+
+                  </div>
+
+              </a>
+          </div>
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  py-4">
+                      <p class="text-dark">پشیبانی ،انتقادات و پیشنهادات </p>
+                      <p></p>
+
+                      <p class="t6 text-danger"><b>___________</b></p>
+                      <p class="text-primary">ایمیل</p>
+
+                  </div>
+
+              </a>
+        </div>
+          <div></div>
+          <div>
+              <h3 class="text-center t6">ما درشبکه های اجتماعی</h3>
+          </div>
+        <div class="row d-flex  pt-4 ">
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  ">
+                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
+                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
+                      
+                      <p class="pt-3 text-danger"><b>سامانه گفتگو انلاین</b></p>
+                      <p class="text-primary">پشتیبانی آنلاین وبسایت</p>
+                      <i>
+                        
+                      </i>
+
+                  </div>
+
+              </a>
+          </div>
+          <div class="col">
+              <a href="#" >
+                  <div class="border border-muted text-center  ">
+
+                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
+                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
+                      
+                      <p class="pt-3 text-danger"><b>@abc-123</b></p>
+                      <p class="text-primary">پشتیبانی تلگرام</p>
+
+                  </div>
+
+              </a>
+          </div>
+         </div> 
+      </div>
+      <div class="text-center t6">
+          <h3>
+              آدرس دفتر مرکزی</h3>
+           <p> بخش مرکزی ، شهر مشهد، محله نیرو هوایی ، کوچه هاشمیه 91/1 ، خیابان هاشمیه 89 ، پلاک -197 ، طبقه چهارم </p>
+
+            <h3>
+                کد پستی:9178194739</h3>
+            <h3> شماره تلفن دفتر مرکزی</h3>
+
+            <p>051-388-333-21 (روز های کاری 9 الی 14)</p>
+
+        <p>* خواهشمندیم جهت پشتیبانی یا پیگیری سفارش با دفتر مرکزی <u>تماس نگیرید </u>*</p>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +127,11 @@ export default {
 </script>
 
 <style>
+.t1{margin-inline: 200px;}
+.t2{margin-inline: 750px;}
+.t3{margin-top: 100px;}
+.t4{margin-top: 200px;}
+.t5{margin-inline: 65px;}
+.t6{margin-top:70px;}
 
 </style>
