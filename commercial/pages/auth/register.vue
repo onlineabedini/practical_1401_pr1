f5da682dc7f07802f30647070245ccf20eac8a44
armina <arminaghanadian@gmail.com>
2022-07-30 13:10:39 +0430

    register.vue completed

--- commercial/pages/auth/register.vue
+++ commercial/pages/auth/register.vue
@@ -1,4 +1,47 @@
 <template>
+<div>
+   <div class="container">
+ <h2 class="mx-4 pt-5 text-center-right container-lg "> ثبت نام</h2>
+    <div class="d-flex">
+       <p class="mx-4 pt-3 text-md-end fs-5"> حساب دارید؟</p> 
+       <p class="mx-2 pt-3 text-md-end fs-5 text-decoration-underline text-danger"> وارد شوید</p>
+    </div>
+    <div class="mb-3 w-50 p-3">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="نام کاربری">
+  <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="آدرس ایمیل">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label"></label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="شماره موبایل">
+</div>
+</div>
+    </div>
+<div class=" row bg-white ">
+<div class="col-2"></div>
+<div class="col-8">
+<p class="mx-4  text-end">ثبت نام با رمز یه بار مصرف</p>
+</div>
+<div class="col-2"></div>
+</div>
+
+
+<div class="d-grid gap-2 col-6 mx-4 w-50 p-3  ">
+  <button class="btn btn-secondary" type="button">ثبت نام با رمز عبور</button>
+  
+  <button class="btn btn-primary" type="button">ورود با گوگل</button>
+</div>
+
+</div>
+
+
+
+
+
+
+
   
 </template>
 
