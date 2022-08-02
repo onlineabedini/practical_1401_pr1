011362910e54232675ce48fd50a9818a78b52551
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-08-02 09:12:30 +0430

    login js code

--- commercial/components/auth/loggin/index.vue
+++ commercial/components/auth/loggin/index.vue
@@ -1,13 +1,22 @@
-<template>
-  
-</template>
+<template></template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      login: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch("login", this.login);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
