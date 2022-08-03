c50fc2fb2a9e7c1ff8ae56243f14f796cc8d3de3
aliqasemii <alighasmi2000@gamil.com>
2022-08-03 19:23:33 +0430

    tak done

--- commercial/pages/page_404.vue
+++ commercial/pages/page_404.vue
@@ -1,5 +1,28 @@
 <template>
-  
+  <div>
+      <div>
+
+      </div>
+      <div class="text-center t1">
+          <h1 class="text-danger fw-bolder">404</h1>
+          
+            <div>
+             <p class="text-body fs-5">
+             صفحه مورد نظر شما یافت نشد.
+             </p>
+            </div>
+            <div class="p-3">
+                <span class="border border-info p-2 rounded-pill bg-primary ">
+                    <a href="#" class="fst-normal text-white text-decoration-none">
+                        بازگشت به صفحه اصلی 
+                     </a>
+                </span>
+            </div>
+         <p class="text-muted fst-normal fs-6 pt-3">
+            از نوار جست و جو بالا برای پیدا کردن کالای مورد نظرتان استفاده نمایید یا کالا های <b class="text-danger ">پر بازدید</b> زیر را ببینید.
+         </p>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +32,6 @@ export default {
 </script>
 
 <style>
+.t1{margin-top: 250px;}
 
 </style>
