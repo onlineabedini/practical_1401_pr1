47a1be9974cef94ea6aa3774201f54a8e25c5d94
armina <arminaghanadian@gmail.com>
2022-08-03 12:20:32 +0430

    panel_menu completed

--- commercial/pages/panel/index.vue
+++ commercial/pages/panel/index.vue
@@ -1,11 +1,20 @@
 <template>
+<div>
+    <panel_menu></panel_menu>
+</div>
   
 </template>
 
 <script>
-export default {
+import panel_menu from '@/components/menu/panel_menu';
 
-}
+
+export default {
+    components: {
+        panel_menu
+    },
+
+};
 </script>
 
 <style>
