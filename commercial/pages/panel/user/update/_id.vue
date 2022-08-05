8d8e6d2cbcf6c2ce004f4951e3b8068b6c45ded1
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-08-05 14:08:18 +0430

    user update primary designs and routings

--- commercial/pages/panel/user/update/_id.vue
+++ commercial/pages/panel/user/update/_id.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-4 p-5">
+        <div class="border shadow-sm p-3 d-grid">
+          <h4 class="text-center">ویرایش اطلاعات کاربری</h4>
+          <br />
+          <input type="text" placeholder="نام" />
+          <br />
+          <input type="text" placeholder=" نام خانوادگی" />
+          <br />
+          <input type="text" placeholder="ایمیل" />
+          <br />
+          <input type="text" placeholder="کلمه عبور" />
+          <br />
+          <input type="text" placeholder="تکرار کلمه عبور" />
+          <br />
+
+          <div class="btn btn-danger text-light d-block">ذخیره تغییرات</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
