8fd2f215f79782bcdd225b6d01abff695c96877b
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-08-05 14:50:19 +0430

    updated prof_edit

--- commercial/pages/panel/user/update/_id.vue
+++ commercial/pages/panel/user/update/_id.vue
@@ -5,18 +5,49 @@
         <div class="border shadow-sm p-3 d-grid">
           <h4 class="text-center">ویرایش اطلاعات کاربری</h4>
           <br />
-          <input type="text" placeholder="نام" />
+          <input
+            type="text"
+            class="form-control"
+            placeholder="نام"
+            v-model="user.name"
+          />
           <br />
-          <input type="text" placeholder=" نام خانوادگی" />
+          <input
+            type="text"
+            placeholder=" نام خانوادگی"
+            class="form-control"
+            v-model="user.family_name"
+          />
           <br />
-          <input type="text" placeholder="ایمیل" />
+          <input
+            type="text"
+            placeholder="ایمیل"
+            class="form-control"
+            v-model="user.email"
+          />
           <br />
-          <input type="text" placeholder="کلمه عبور" />
+          <input
+            type="password"
+            placeholder="کلمه عبور"
+            class="form-control"
+            v-model="user.pass"
+          />
           <br />
-          <input type="text" placeholder="تکرار کلمه عبور" />
+          <input
+            type="password"
+            placeholder="تکرار کلمه عبور"
+            class="form-control"
+            v-model="user.repeate_pass"
+          />
           <br />
 
-          <div class="btn btn-danger text-light d-block">ذخیره تغییرات</div>
+          <div
+            class="btn btn-danger text-light d-block"
+            type="submit"
+            @click="login()"
+          >
+            ذخیره تغییرات
+          </div>
         </div>
       </div>
     </div>
@@ -24,7 +55,29 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        family_name: "",
+        email: "",
+        pass: "",
+        repeate_pass: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log("username:", this.user.name);
+      console.log("family_name:", this.user.family_name);
+      console.log("email:", this.user.email);
+      console.log("pass:", this.user.pass);
+      console.log("repeate_pass:", this.user.repeate_pass);
+      alert(" تغیر اطلاعات موفقیت آمیز بود");
+    },
+  },
+};
 </script>
 
 <style>
