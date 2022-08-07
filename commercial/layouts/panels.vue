b21a70b44ea272095a8683ec5909db3aa36d3e38
soroosh aghaei <soroosh77aghaei@gmail.com>
2022-08-07 12:39:03 +0430

    layout for panel page was added. not completed yet

--- commercial/layouts/panels.vue
+++ commercial/layouts/panels.vue
@@ -1,11 +1,18 @@
 <template>
-  <Nuxt />
+  <div>
+    <menu_panels/>
+    <Nuxt />
+  </div>
 </template>
 
 <script>
+import menu_panels from '@/components/menu/panel_menu/index.vue';
 export default {
-
-}
+  components: {
+    menu_panels,
+  },
+   
+};
 </script>
 
 <style>
--- commercial/pages/panel/index.vue
+++ commercial/pages/panel/index.vue
@@ -1,18 +1,15 @@
 <template>
 <div>
-    <panel_menu></panel_menu>
+  
 </div>
   
 </template>
 
 <script>
-import panel_menu from '@/components/menu/panel_menu';
 
 
 export default {
-    components: {
-        panel_menu
-    },
+    layout: 'panels',
 
 };
 </script>
