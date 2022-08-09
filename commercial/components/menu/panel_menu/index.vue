090d6709ed9511bfe6443022c9bb0addbdd19ee2
armina <arminaghanadian@gmail.com>
2022-08-09 21:50:46 +0430

    panel completed

--- commercial/components/menu/panel_menu/index.vue
+++ commercial/components/menu/panel_menu/index.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
     <div class="p-4 border border-2 m-3">
-      <a class="d-flex text-decoration-none text-muted my-2" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark my-2" href="#">
+        <i class="fa fa-tachometer-alt fa-2x ms-3"></i>
+        <p class="mt-1">داشبورد</p>
       </a>
-      <a class="d-flex text-decoration-none text-muted" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark" href="#">
+        <i class="fa fa-address-book fa-2x ms-3"></i>
+        <p class="mt-1">حساب کاربری</p>
       </a>
-      <a class="d-flex text-decoration-none text-muted my-2" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark my-2" href="#">
+        <i class="fa fa-user-alt fa-2x ms-3"></i>
+        <p class="mt-1">کاربران</p>
       </a>
-      <a class="d-flex text-decoration-none text-muted" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark my-2" href="#">
+        <i class="fa fa-bullhorn fa-2x ms-3"></i>
+        <p class="mt-1">اعلانات</p>
       </a>
-      <a class="d-flex text-decoration-none text-muted my-2" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark my-2" href="#">
+        <i class="fa fa-clone fa-2x ms-3"></i>
+        <p class="mt-1">محصولات </p>
       </a>
-      <a class="d-flex text-decoration-none text-muted" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark" href="#">
+        <i class="fa fa-images fa-2x ms-3"></i>
+        <p class="mt-1">پست ها</p>
       </a>
-      <a class="d-flex text-decoration-none text-muted my-2" href="#">
-        <i class="fa fa-user fa-2x ms-3"></i>
-        <p class="mt-1">user</p>
+      <a class="d-flex text-decoration-none text-dark my-2" href="#">
+        <i class="fa fa-power-off fa-2x ms-3"></i>
+        <p class="mt-1">خروج</p>
       </a>
     </div>
   </div>
