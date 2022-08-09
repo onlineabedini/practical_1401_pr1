3e4dc386f7c595fbec58e4d562b6ec00f941bca0
armina <arminaghanadian@gmail.com>
2022-08-09 19:33:25 +0430

    panel completed

--- commercial/components/menu/panel_menu/index.vue
+++ commercial/components/menu/panel_menu/index.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
-    <div>
+   <div class="  row bg-white pt-5 mx-4 my-2 d-flex justify-content-end shadow-sm p-3 mb-5 bg-body rounded ">
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom ">
+       <i class="fas fa-tachometer-alt"></i><span>داشبورد</span>
+     </a>
+     
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom  ">
+       <i class="fas fa-address-book"></i><span>حساب کاربری</span>
+     </a>
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom   ">
+       <i class="fas fa-user-alt"></i><span>کاربران</span>
+     </a>
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom  ">
+       <i class="fas fa-clone"></i><span>محصولات</span>
+     </a>
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom  ">
+       <i class="fas fa-bullhorn"></i><span>اعلانات</span>
+     </a>
+     <a href="#" class="text-decoration-none text-dark mx-2 pt-2 border-bottom">
+       <i class="fas fa-images"></i><span>پست ها</span>
+       
+     </a>
+   </div>
     </div>
-  </div>
+  
 </template>
 
 <script>
