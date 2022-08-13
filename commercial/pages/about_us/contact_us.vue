e69d3b75f98d8a11ca7149d8668868ef43a76e80
aliqasemii <alighasmi2000@gamil.com>
2022-08-13 16:30:10 +0430

    SEO of pages was done

--- commercial/pages/about_us/contact_us.vue
+++ commercial/pages/about_us/contact_us.vue
@@ -1,135 +1,113 @@
 <template>
-  <div>
-      <div>
-          <h1 class="text-center pt-5">
-              ارتباط با ما
-          </h1>
-      </div>
-      <div >
-          <p class="text-center pt-5 t1">
-              در راستای ارتقا سطح کیفیت و پشتبانی وبسایت و همچنین ماهیت محصولات ارایه شده ، تصمیم گرفتیم تا پشتیبانی آنلاین وبسایت را بصورت 12 ساعته راه اندازی کنیم و از این پس می توانید برای حل مشکلات خود از طریق سامانه <strong>پشتیبانی</strong> آنلاین سایت خدمات خود را دریافت نمایید.همچنین اگر بر حسب اتفاق سامانه چت آنلاین با نقص فنی مواجه شد می توانید از طریق راه های ارتباطی زیر با ما در تماس باشید ، همکاران ما بی درنگ پاسخگوی شما خواهند بود.
-          </p>
-      </div>
-      <div class="row d-flex t4 px-5 t5">
-          <div class="col">
-              <a href="#" class="text-decoration-none" >
-                  <div class="border border-muted text-center py-4 rounded">
-                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
-                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
-                      
-                      <p class="pt-3 text-danger"><b>سامانه گفتگو انلاین</b></p>
-                      <p class="text-primary">پشتیبانی آنلاین وبسایت</p>
-                      <div> <i class="far fa-comment-alt"  style="font-size:48px;color:black"></i></div>
-                  </div>
-
-              </a>
-          </div>
-          <div class="col">
-              <a href="#" class="text-decoration-none" >
-                  <div class="border border-muted text-center  py-4 rounded">
-
-                      <p class="text-dark">پاسخگویی به سوالات و مشکلات فنی</p>
-                      <p class="text-dark">(همه روز از ساعت 9 تا 21)</p>
-                      
-                      <p class="pt-3 text-danger"><b>@abc-123</b></p>
-                      <p class="text-primary">پشتیبانی تلگرام</p>
-                      <div> <i class="fab fa-telegram-plane"  style="font-size:48px;color:black"></i></div>
-
-                  </div>
-
-              </a>
-          </div>
-          <div class="col">
-              <a href="#" class="text-decoration-none" >
-                  <div class="border border-muted text-center  py-4 rounded ">
-                      <p class="text-dark"> پشتیبانی سفارش، انتقادات و پیشنهادات </p>
-                      <p class="text-dark">(روز های کاری 9 الی 14)</p>
-
-                      <p class="pt-3 text-danger"> <b>00 00 0000_031</b></p>
-                      <p class="text-primary">11 11 1111_031</p>
-                      <div> <i class="fa fa-phone" style="font-size:48px;color:black"></i></div>
-
-                  </div>
-
-              </a>
-          </div>
-          <div class="col">
-              <a href="#" class="text-decoration-none">
-                  <div class="border border-muted text-center  py-4 rounded">
-                      <p class="text-dark">پشیبانی ،انتقادات و پیشنهادات </p>
-                      <p></p>
-
-                      <p class="t6 text-danger"><b>___________</b></p>
-                      <p class="text-primary">ایمیل</p>
-                      <div> <i class="fa fa-envelope" style="font-size:48px;color:black"></i></div>
-
-                  </div>
-
-              </a>
+    <div>
+        <div class="text-center pt-5">
+            <h1>
+                ارتباط با ما
+            </h1>
         </div>
-          <div></div>
-          <div>
-              <h3 class="text-center t6">ما درشبکه های اجتماعی</h3>
-          </div>
-        <div class="row d-flex  pt-4 ">
-          <div class="col">
-              <a href="#" class="text-decoration-none">
-                  <div class="border border-muted text-center py-3 px-3 rounded w-auto">
-
-                      <div><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+        <div class="text-center pt-5 t1">
+            <p>
+                در راستای ارتقا سطح کیفیت و پشتبانی وبسایت و همچنین ماهیت محصولات ارایه شده ، تصمیم گرفتیم تا پشتیبانی
+                آنلاین وبسایت را بصورت 12 ساعته راه اندازی کنیم و از این پس می توانید برای حل مشکلات خود از طریق سامانه
+                <strong>پشتیبانی</strong> آنلاین سایت خدمات خود را دریافت نمایید.همچنین اگر بر حسب اتفاق سامانه چت
+                آنلاین با نقص فنی مواجه شد می توانید از طریق راه های ارتباطی زیر با ما در تماس باشید ، همکاران ما بی
+                درنگ پاسخگوی شما خواهند بود.
+            </p>
+        </div>
+        <div class="row d-flex t4 px-5 t5">
+            <div class="col">
+                <a href="https://support.license-market.ir/" class="text-decoration-none">
+                    <div class="border border-muted text-center py-4 rounded text-dark">
+                        <p>پاسخگویی به سوالات و مشکلات فنی</p>
+                        <p>(همه روز از ساعت 9 تا 21)</p>
+                        <p class="pt-3 text-danger"><b>سامانه گفتگو انلاین</b></p>
+                        <p class="text-primary">پشتیبانی آنلاین وبسایت</p>
+                        <div> <i class="far fa-comment-alt font1 color1"></i></div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="https://cdn.license-market.ir/blog/channel/" class="text-decoration-none">
+                    <div class="border border-muted text-center  py-4 rounded text-dark">
+                        <p>پاسخگویی به سوالات و مشکلات فنی</p>
+                        <p>(همه روز از ساعت 9 تا 21)</p>
+                        <p class="pt-3 text-danger"><b>@abc-123</b></p>
+                        <p class="text-primary">پشتیبانی تلگرام</p>
+                        <div> <i class="fab fa-telegram-plane font1 color1"></i></div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="border border-muted text-center  py-4 rounded text-dark">
+                        <p> پشتیبانی سفارش، انتقادات و پیشنهادات </p>
+                        <p>(روز های کاری 9 الی 14)</p>
+                        <p class="pt-3 text-danger"> <b>00 00 0000_031</b></p>
+                        <p class="text-primary">11 11 1111_031</p>
+                        <div> <i class="fa fa-phone font1 color1"></i></div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="border border-muted text-center  py-4 rounded">
+                        <p class="text-dark">پشیبانی ،انتقادات و پیشنهادات </p>
+                        <p class="t6 text-danger"><b>___________</b></p>
+                        <p class="text-primary">ایمیل</p>
+                        <div> <i class="fa fa-envelope font1 color1"></i></div>
+                    </div>
+                </a>
+            </div>
+            <div><h3 class="text-center t6">ما درشبکه های اجتماعی</h3></div>
+            <div class="row d-flex  pt-4 ">
+                <div class="col">
+                    <a href="https://www.instagram.com/licensemarket/" class="text-decoration-none">
+                        <div class="border border-muted text-center py-3 px-3 rounded w-auto">
+                            <div><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor"
+                                    class="bi bi-instagram" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
                                 </svg></div>
-                      <p class="text-dark pt-3">آگاهی از آخرین محصولات، کد های تخفیف، آموزش ها و ترفند سرویس ها</p>
-                      <p class="pt-3 text-danger"><b>@--------</b></p>
-
-                  </div>
-
-              </a>
-          </div>
-          <div class="col">
-              <a href="#" class="text-decoration-none">
-                  <div class="border border-muted text-center py-3 px-3 rounded w-auto ">
-                      
-                      <div> <i class="fab fa-telegram-plane"  style="font-size:48px;color:blue"></i></div>
-
-                      <p class="text-dark pt-3">آگاهی از آخرین محصولات و ثبت سفارش محصولاتی که در سایت موجود نمی باشند.</p>
-                      <p class="pt-3 text-danger"><b>@-------</b></p>
-                  </div>
-
-              </a>
-          </div>
-         </div> 
-      </div>
-      <div class="text-center t6">
-          <h3>
-              آدرس دفتر مرکزی</h3>
-           <p> بخش مرکزی ، شهر مشهد، محله نیرو هوایی ، کوچه هاشمیه 91/1 ، خیابان هاشمیه 89 ، پلاک -197 ، طبقه چهارم </p>
-
-            <h5>
-                کد پستی:9178194739</h5>
+                            <p class="text-dark pt-3">آگاهی از آخرین محصولات، کد های تخفیف، آموزش ها و ترفند سرویس ها
+                            </p>
+                            <p class="pt-3 text-danger"><b>@--------</b></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://cdn.license-market.ir/blog/channel/" class="text-decoration-none">
+                        <div class="border border-muted text-center py-3 px-3 rounded w-auto ">
+                            <div> <i class="fab fa-telegram-plane font1 color2"></i></div>
+                            <p class="text-dark pt-3">آگاهی از آخرین محصولات و ثبت سفارش محصولاتی که در سایت موجود نمی
+                                باشند.</p>
+                            <p class="pt-3 text-danger"><b>@-------</b></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="text-center t6">
+            <h3>آدرس دفتر مرکزی</h3>
+            <p> بخش مرکزی ، شهر مشهد، محله نیرو هوایی ، کوچه هاشمیه 91/1 ، خیابان هاشمیه 89 ، پلاک -197 ، طبقه چهارم
+            </p>
+            <h5>کد پستی:9178194739</h5>
             <h3 class=" pt-5"> شماره تلفن دفتر مرکزی</h3>
-
             <p>051-388-333-21 (روز های کاری 9 الی 14)</p>
-
-        <p>* خواهشمندیم جهت پشتیبانی یا پیگیری سفارش با دفتر مرکزی <u>تماس نگیرید </u>*</p>
-      </div>
-  </div>
+            <p>* خواهشمندیم جهت پشتیبانی یا پیگیری سفارش با دفتر مرکزی <u>تماس نگیرید </u>*</p>
+        </div>
+    </div>
 </template>
-
 <script>
-export default {
-
-}
+export default {}
 </script>
-
 <style scoped>
-.t1{margin-inline: 200px;}
-.t2{margin-inline: 750px;}
-.t3{margin-top: 100px;}
-.t4{margin-top: 200px;}
-.t5{margin-inline: 65px;}
-.t6{margin-top:70px;}
-
+.t1 {margin-inline: 200px;}
+.t2 {margin-inline: 750px;}
+.t3 {margin-top: 100px;}
+.t4 {margin-top: 200px;}
+.t5 {margin-inline: 65px;}
+.t6 {margin-top: 70px;}
+.font1{font-size: 48px;}
+.color1{color:black;}
+.color2{color:blue}
 </style>
-
-
