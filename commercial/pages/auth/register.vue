41adfe129fd7dab57b3768e9f9cf717931e3a873
Asnasr99udkc <saba.nasr2019@gmail.com>
2022-08-16 22:06:15 +0430

    Update register.vue

--- commercial/pages/auth/register.vue
+++ commercial/pages/auth/register.vue
@@ -8,28 +8,28 @@
       </div>
       <div class="mb-3 w-50 p-3 ">
         <label for="exampleFormControlInput1" class="form-label"></label>
-        <input type="text" class="form-control" v-modal="first_name" id="exampleFormControlInput1" placeholder="نام" />
+        <input type="text" class="form-control" v-model="user_data.first_name" id="exampleFormControlInput1" placeholder="نام" />
         <div class="mb-3 ">
           <label for="exampleFormControlInput1" class="form-label"></label>
-          <input type="text" class="form-control" v-modal="last_name" id="exampleFormControlInput1"
+          <input type="text" class="form-control" v-model="user_data.last_name" id="exampleFormControlInput1"
             placeholder=" نام خانوادگی" />
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label"></label>
-            <input type="email" class="form-control" v-modal="email" id="exampleFormControlInput1"
+            <input type="email" class="form-control" v-model="user_data.email" id="exampleFormControlInput1"
               placeholder="ایمیل" />
           </div>
           <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label"></label>
-            <input type="password" class="form-control" v-modal="password" id="exampleFormControlInput1"
+            <input type="password" class="form-control" v-model="user_data.password" id="exampleFormControlInput1"
               placeholder="پسورد" />
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label"></label>
-              <input type="address" class="form-control" v-modal="address" id="exampleFormControlInput1"
+              <input type="address" class="form-control" v-model="user_data.address" id="exampleFormControlInput1"
                 placeholder="ادرس" />
             </div>
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label"></label>
-              <input type="email" class="form-control" v-modal="phone_number" id="exampleFormControlInput1"
+              <input type="number" class="form-control" v-model="user_data.phone_number" id="exampleFormControlInput1"
                 placeholder="شماره موبایل" />
             </div>
           </div>
@@ -37,7 +37,7 @@
       </div>
     </div>
     <div class="d-grid gap-2 col-6 mx-4 w-50 p-3 ">
-      <button class="btn btn-primary" type="button" @click="send_data()">ثبت نام</button>
+      <button class="btn btn-primary" type="button" @click="submit()">ثبت نام</button>
     </div>
   </div>
 </template>
@@ -56,8 +56,16 @@ export default {
     };
   },
   methods: {
-    send_data() {
-      console.log(this.user)
+    submit() {
+      this.$axios
+        .post("", this.user_data)
+        .then((response)=>{
+          console.log(response.user_data)
+          this.user_data=response.data;
+    })
+        .catch((error)=>{
+          console.log(error);
+        }) 
       alert('ثبت نام انجام شد')
       // axios => send to backend 
       // response <= backend
@@ -65,4 +73,5 @@ export default {
   }
 };
 </script>
-<style></style>
+<style>
+</style>
