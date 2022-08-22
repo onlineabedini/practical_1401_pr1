a70c925ca2a5762854b5d67a4a57e3ed873c39e2
Asnasr99udkc <saba.nasr2019@gmail.com>
2022-08-23 03:34:10 +0430

    Update _id.vue

--- commercial/pages/panel/user/update/_id.vue
+++ commercial/pages/panel/user/update/_id.vue
@@ -5,15 +5,15 @@
         <div class="border shadow-sm p-3 d-grid">
           <h4 class="text-center">ویرایش اطلاعات کاربری</h4>
           <br />
-          <input type="text" class="form-control" placeholder="نام" v-model="user.name" />
+          <input type="text" class="form-control my-3" placeholder="نام" v-model="user_data.first_name" />
           <br />
-          <input type="text" placeholder=" نام خانوادگی" class="form-control" v-model="user.family_name" />
+          <input type="text" placeholder=" نام خانوادگی" class="form-control my-3" v-model="user_data.last_name" />
           <br />
-          <input type="text" placeholder="ایمیل" class="form-control" v-model="user.email" />
+          <input type="text" placeholder="ایمیل" class="form-control my-3" v-model="user_data.email" />
           <br />
-          <input type="password" placeholder="کلمه عبور" class="form-control" v-model="user.pass" />
+          <input type="password" placeholder="کلمه عبور" class="form-control my-3" v-model="user_data.password" />
           <br />
-          <input type="password" placeholder="تکرار کلمه عبور" class="form-control" v-model="user.repeate_pass" />
+          <input type="password" placeholder="تایید کلمه عبور" class="form-control my-3" v-model="user_data.confirm_password" />
           <br />
           <div class="btn btn-danger text-light d-block" type="submit" @click="login()">ذخیره تغییرات</div>
         </div>
@@ -22,6 +22,8 @@
   </div>
 </template>
 <script>
+import { all } from 'q';
+
 export default {
   data() {
     return {
@@ -36,14 +38,22 @@ export default {
   },
   methods: {
     login() {
-      console.log("username:", this.user.name);
-      console.log("family_name:", this.user.family_name);
-      console.log("email:", this.user.email);
-      console.log("pass:", this.user.pass);
-      console.log("repeate_pass:", this.user.repeate_pass);
+      this.$axios
+        .get("http://localhost:8080/api/users/all", this.user_data)
+        .then((response) => {
+          console.log(response.user_data);
+          this.user_data=response.data;
+          })
+        .update("http://localhost:8080/api/user/update", this.user_data)
+        .then((response) => {
+          console.log(response.user_data);
+          this.user_data=response.data;
+          });
+        
       alert(" تغیر اطلاعات موفقیت آمیز بود");
     },
   },
 };
 </script>
-<style></style>
+<style>
+</style>
