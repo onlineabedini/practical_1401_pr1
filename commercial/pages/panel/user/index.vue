1bedf0d67cf08d52afab6411cc574953bb51ae69
Asnasr99udkc <saba.nasr2019@gmail.com>
2022-08-27 16:34:51 +0430

    update

--- commercial/pages/panel/user/index.vue
+++ commercial/pages/panel/user/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    
-  </div>
+  
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style>
--- commercial/pages/panel/user/update/_id.vue
+++ commercial/pages/panel/user/update/_id.vue
@@ -5,17 +5,49 @@
         <div class="border shadow-sm p-3 d-grid">
           <h4 class="text-center">ویرایش اطلاعات کاربری</h4>
           <br />
-          <input type="text" class="form-control my-3" placeholder="نام" v-model="user_data.first_name" />
+          <input 
+            type="text" 
+            class="my-3"
+            placeholder="نام"
+            v-model="user_data_update.name"
+          />
           <br />
-          <input type="text" placeholder=" نام خانوادگی" class="form-control my-3" v-model="user_data.last_name" />
+          <input 
+            type="text"
+            placeholder=" نام خانوادگی" 
+            class="my-3" 
+            v-model="user_data_update.last_name" 
+           />
           <br />
-          <input type="text" placeholder="ایمیل" class="form-control my-3" v-model="user_data.email" />
+          <input 
+           type="text"
+           placeholder="ایمیل" 
+           class="my-3" 
+           v-model="user_data_update.email" 
+          />
           <br />
-          <input type="password" placeholder="کلمه عبور" class="form-control my-3" v-model="user_data.password" />
+          <input 
+           type="text"
+           placeholder="شماره تلفن همراه"
+           class="my-3" 
+           v-model="user_data_update.email" 
+          />
           <br />
-          <input type="password" placeholder="تایید کلمه عبور" class="form-control my-3" v-model="user_data.confirm_password" />
+          <input 
+            type="password"
+            placeholder="کلمه عبور" 
+            class="my-3" 
+            v-model="user_data_update.password"
+          />
           <br />
-          <div class="btn btn-danger text-light d-block" type="submit" @click="login()">ذخیره تغییرات</div>
+          <input
+            type="password"
+            placeholder="کلمه عبور جدید"
+            class="my-3"
+            v-model="user_data_update.confirm_password" 
+            />
+          <br />
+          <div class="btn btn-danger text-light d-block" type="submit" @click="update()">ذخیره تغییرات</div>
         </div>
       </div>
     </div>
@@ -25,31 +57,39 @@
 export default {
   data() {
     return {
-      id:1,
-      user_data: {
-        first_name:"",
+       id:1,
+       user_data: {
+        name:"",
         last_name: "",
         email: "",
+        phone_number:"",
         password: "",
-        confirm_password: "",
+        new_password: "",
       },
+      confirm_pass:"",
     };
   },
   mounted(){
     this.id = this.$route.params.id
   },
   methods:{
+    get_user() {
+      this.$axios
+      .get("http://localhost:8080/api/users/all" ,this.user_data_update)
+      .then((response) => {
+        this.user_data_update=response.data;
+          console.log(this.user_data_update);
+        })
+      .catch(error => consel.log(error));
+    },
     update() {
-      this.$axios.put("" +this.id).then(
-        response => {
-          console.log(response)
-        }
-      )
-      .catch(
-        error => {
-          consel.log(error)
-        }
-      )
+      this.$axios
+      .put("http://localhost:8080/api/user/update" ,this.user_data_update)
+      .then((response) => {
+        this.user_data_update=response.data;
+          console.log(this.user_data_update);
+        })
+      .catch(error => consel.log(error));
     },
   }
 };
