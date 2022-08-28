a0549c4be1416fa6a8562dd79b60d99097306002
armina <arminaghanadian@gmail.com>
2022-08-28 15:07:38 +0430

    index warning completed

--- commercial/pages/panel/warning/index.vue
+++ commercial/pages/panel/warning/index.vue
@@ -1,41 +1,52 @@
 <template>
-<div>
-  <div class=" p-4 border border-3 m-3 pt-5   ">
-    <a class="d-flex text-decoration-none text-dark my-2 border-bottom w-25  " href="#">
-      <p></p>
-         </a>
-         <a class="d-flex text-decoration-none text-dark my-2 border-bottom w-25 " href="#">
-           <p></p>
-         </a>
-         <a class="d-flex text-decoration-none text-dark my-2 border-bottom w-25 " href="#">
-           <p></p>
-         </a>
-         <a class="d-flex text-decoration-none text-dark my-2 border-bottom w-25 " href="#">
-           <p></p>
-         </a>
-    
+  <div>
+    <div class="p-4 border border-3 m-3 pt-5">
+      <a
+        class="d-flex text-decoration-none text-dark my-2 border-bottom w-25"
+        href="#"
+      >
+        <p></p>
+      </a>
+      <a
+        class="d-flex text-decoration-none text-dark my-2 border-bottom w-25"
+        href="#"
+      >
+        <p></p>
+      </a>
+      <a
+        class="d-flex text-decoration-none text-dark my-2 border-bottom w-25"
+        href="#"
+      >
+        <p></p>
+      </a>
+      <a
+        class="d-flex text-decoration-none text-dark my-2 border-bottom w-25"
+        href="#"
+      >
+        <p></p>
+      </a>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
   data() {
- return{
-     warning:[],
+    return {
+      warning: [],
       id: 1,
       alert: {},
     };
   },
-  
+
   methods: {
     all_alert() {
       this.$axios
         .get("")
-        .then(this.warning=response.data) 
-        
+        .then((this.warning = response.data))
+
         .catch((erorr) => {
-       consol.log(erorr);
+          consol.log(erorr);
         });
     },
   },
